soyabkhanmalek/android_device_xiaomi_sunny

sunny: Migrate sensor HAL to AIDL interface
Bug: 247478991
Test: tested on-device (P23) ab/P46941077
Test: `sensor_test -ahal list` and `sensor_test list`
Test: `sensor_test -ahal sample -s0` and `sensor_test sample -s0`

ralph950412: Implementation for Xiaomi devices
Test: m, boot, and test sensors functionality

Change-Id: I40f6ab6b7780c53cd638a1f77c95f6c324dc2d15
Signed-off-by: ralph950412 <ralph950412@gmail.com>
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

device.mk
@@ -230,7 +230,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.xiaomi-multihal
+    android.hardware.sensors-service.xiaomi-multihal
 
 PRODUCT_PACKAGES += \
     sensors.xiaomi

eklerismunir/device_xiaomi_sunny

device.mk
@@ -448,7 +448,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.sunny-multihal
+    android.hardware.sensors-service.sunny-multihal
 
 PRODUCT_PACKAGES += \
     sensors.xiaomi